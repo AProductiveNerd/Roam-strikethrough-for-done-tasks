481f2bbc70f564c75e63f81fad26aa6dfadca1a7
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-10-26 16:42:56 +0530

    Revert "Bug fix?"

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-(() => {
-  setInterval(() => {
-    const checkObj = document.querySelectorAll("[data-page-links*='\"DONE\"']");
+setInterval(() => {
+  const checkObj = document.querySelectorAll("[data-page-links*='\"DONE\"']");
 
-    for (let i = 0; i < checkObj.length; i++) {
-      checkObj[i].style.textDecoration = "line-through";
-    }
+  for (let i = 0; i < checkObj.length; i++) {
+    checkObj[i].style.textDecoration = "line-through";
+  }
 
-    const kanbans = document.getElementsByClassName("kanban-card");
-    const archives = document.getElementsByClassName(
-      "rm-xparser-default-ARCHIVED"
-    );
+  const kanbans = document.getElementsByClassName("kanban-card");
+  const archives = document.getElementsByClassName(
+    "rm-xparser-default-ARCHIVED"
+  );
 
-    for (let i = 0; i < kanbans.length; i++) {
-      if (
-        kanbans[i].children[0].children[0].children[0].children[0].children[0]
-          ?.checked
-      ) {
-        kanbans[i].style.textDecoration = "line-through";
-      }
+  for (let i = 0; i < kanbans.length; i++) {
+    if (
+      kanbans[i].children[0].children[0].children[0].children[0].children[0]
+        ?.checked
+    ) {
+      kanbans[i].style.textDecoration = "line-through";
     }
-    for (let i = 0; i < archives.length; i++) {
-      archives[
-        i
-      ].parentElement.parentElement.parentElement.parentElement.style.textDecoration =
-        "line-through";
-    }
-  }, 3000);
-})();
+  }
+  for (let i = 0; i < archives.length; i++) {
+    archives[
+      i
+    ].parentElement.parentElement.parentElement.parentElement.style.textDecoration =
+      "line-through";
+  }
+}, 3000);
